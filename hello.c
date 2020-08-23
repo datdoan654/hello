e766d9dbe759bc98fffa2d9c2a211e88b3a18e6b
Dat Doan <ddoan4@huskers.unl.edu>
2020-08-23 03:58:32 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dat Doan
+ *         ddoan4@huskers.unle.edu
+ * Date 2020/08/22
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Dat Doan\n");
+    printf("electrical engineering\n");
+    
+    return 0;
+}
