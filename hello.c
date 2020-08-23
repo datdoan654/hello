d68b494d8115fae1d7475508d7abf89c3ab7a388
Angela Palmquist <apalmquist3@huskers.unl.edu>
2020-08-23 14:58:08 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
  * Author: Dat Doan
  *         ddoan4@huskers.unle.edu
  * Date 2020/08/22
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
@@ -11,6 +11,8 @@
 int main(int argc, char **argv) {
     printf("Dat Doan\n");
     printf("electrical engineering\n");
-    
+    printf("Angela Palmquist!\n");
+    printf("Computer Engineering\n");
+
     return 0;
 }
